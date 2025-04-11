e6625750928444f7443f3421c6099df3f345ed73
Aditya Kacha <adityakacha@Adityas-MacBook-Air.local>
2025-04-11 11:20:47 +0530

    Updated Header

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -6,7 +6,18 @@ const BlogList = () => {
   return (
     <section className="relative py-20 px-6 bg-neutral-950 text-white overflow-hidden">
       <div className="relative z-10 max-w-6xl mx-auto">
-        <h2 className="text-3xl font-semibold text-center mb-12">Latest Posts</h2>
+      
+      <div className="relative text-center mb-12">
+        <div className="absolute inset-0 blur-2xl bg-gradient-to-r from-purple-600 via-pink-500 to-indigo-500 opacity-20 rounded-full w-1/2 mx-auto h-12 top-1/2 -translate-y-1/2"></div>
+        <Motion.h2
+          className="relative text-4xl md:text-5xl font-extrabold text-white"
+          initial={{ opacity: 0, y: -30, scale: 0.9 }}
+          animate={{ opacity: 1, y: 0, scale: 1 }}
+          transition={{ duration: 0.8, ease: 'easeOut' }}
+        >
+          Latest Posts
+        </Motion.h2>
+      </div>
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
           {Blogs.map((post, idx) => (
             <Motion.div
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,32 @@
+"use client";
+import React from "react";
+import { motion as Motion} from "motion/react";
+import { LampContainer } from "./ui/lamp";
+ 
 const Hero = () => {
     return (
-      <section className="bg-gradient-to-r from-green-600 to-indigo-700 text-white py-20 px-6 text-center">        
-        <h1 className="text-4xl md:text-5xl font-bold mb-4">
-          AI Tools
-        </h1>
+      <>
+      {/* // <section className="bg-gradient-to-r from-green-600 to-indigo-700 text-white py-20 px-6 text-center">         */}
+       <LampContainer>
+        <Motion.h1
+          initial={{ opacity: 0.5, y: 0 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+        >
+        AI Tools
         <p className="text-lg md:text-xl max-w-2xl mx-auto">
           Discover the smartest tools to boost your productivity and earn more with AI.
         </p>
-      </section>
+        </Motion.h1>
+        </LampContainer>
+      {/* // </section> */}
+      </>
     )
   }
   
-  export default Hero
+export default Hero
